8a19479dc2cdf5170d9291321a3493e0fcdf7a94
Helder <h.roque1987@gmail.com>
2022-07-19 20:20:04 +0100

    Second

---
+++ Models/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        //Relatonship
+
+        public List <Movie> Movies{ get; set; }
+
+    }
+}
